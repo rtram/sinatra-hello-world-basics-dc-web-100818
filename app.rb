90d5506aaf42c1e12a14849fdfed203a3231c4ea
Robin Tram <robin.tram@gmail.com>
2018-10-28 12:41:34 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,24 @@
 class App < Sinatra::Base
 
-  # Define your GET '/' route below and respond with "Hello, World!"
+  get '/' do
+    "Hello, World!"
+
+  end
 
 
 end
+
+#
+# describe "GET '/'" do
+#   before do
+#     get '/'
+#   end
+#
+#   it "responds with a 200 status code" do
+#     expect(last_response.status).to eq(200)
+#   end
+#
+#   it "responds with 'Hello, World!'" do
+#     expect(last_response.body).to include("Hello, World!")
+#   end
+# end
